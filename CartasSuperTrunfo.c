d25457ecb7e986fd0e5087194f8d6b0d0bd02583
Kauancode12 <kakabranco39@gmail.com>
2025-03-02 14:05:59 +0000

    funçoes printf e scanf adicionadas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,26 @@ int main(){
     float area;
     float pib;
 
+    printf("Estado: \n");
+    scanf("%s", &estado);
+
+    printf("Cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("População: \n");
+    scanf("%d", &populacao);
+
+    printf("Número de pontos turísticos: \n");
+    scanf("%d", &pontos_turisticos);
+
+    printf("Área em km²: \n");
+    scanf("%f", &area);
+
+    printf("PIB: \n");
+    scanf("%f", &pib);
+
+    
+
    
 
 
